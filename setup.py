bae84228c62372e91e863f9a1cce979cf75ca345
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2017-10-04 21:55:45 +0200

    fixed

--- setup.py
+++ setup.py
@@ -16,12 +16,11 @@ setup(
     name = "jwt-generator",
     packages = ["generator"],
     entry_points = {
-        "console_scripts": ['jwt-generator = generator.generator:main']
+        "console_scripts": ['jwt-generator = generator.jwt_generator:main']
         },
     version = version,
     description = "a simple JWT generator.",
     long_description='todo',
-    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
     platforms='any',
