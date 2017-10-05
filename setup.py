7f557f09c10c65c6d28b0c9fd7f22ad3f12f7c36
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2017-10-05 09:41:02 +0200

    bumped to version 0.1.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ dependencies = open(path.join(here, 'requirements.txt'),'r').readlines()
 with open(path.join(here, 'README.md'), 'r') as f:
     long_description = f.read()
 
-version="1.0"
+version="0.1.6"
 
 setup(
     name = "jwt-generator",
